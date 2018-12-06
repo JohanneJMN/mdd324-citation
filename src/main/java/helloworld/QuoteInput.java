f2683f2e657c4e87f7a26e008aa0b4407d24788b
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-12-06 16:31:33 +0100

    Initial commit

---
+++ src/main/java/helloworld/QuoteInput.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class QuoteInput {
+    private Contents contents;
+
+    public Contents getContents() {
+        return contents;
+    }
+
+    public void setContents(Contents contents) {
+        this.contents = contents;
+    }
+}
